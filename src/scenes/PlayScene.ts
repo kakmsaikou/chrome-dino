ba607b8cb12808910606b6b69d173be0804017f7
TacitusPie <kakmsaikou@gmail.com>
2022-10-04 07:18:21 +0800

    完成加载小恐龙

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -2,6 +2,7 @@ import * as Phaser from 'phaser';
 
 export class PlayScene extends Phaser.Scene {
   private ground: Phaser.GameObjects.TileSprite;
+  private dino: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 
   constructor() {
     super('PlayScene');
@@ -10,8 +11,14 @@ export class PlayScene extends Phaser.Scene {
   create(): void {
     const {width, height} = this.game.config;
 
+    // 加载地图资源
     this.ground = this.add
       .tileSprite(0, height as number, width as number, 26, 'ground')
+      .setOrigin(0, 1); // 把地图原点设为左下角
+
+    // 加载小恐龙
+    this.dino = this.physics.add
+      .sprite(0, height as number, 'dino-idle')
       .setOrigin(0, 1);
   }
 }

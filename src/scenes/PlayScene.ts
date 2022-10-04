3f15f7a8d238e1b3f4c021849538cdd5271e38de
TacitusPie <kakmsaikou@gmail.com>
2022-10-04 08:10:58 +0800

    小恐龙能下蹲了/地图能滚动了

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -3,9 +3,12 @@ import * as Phaser from 'phaser';
 export class PlayScene extends Phaser.Scene {
   private ground: Phaser.GameObjects.TileSprite;
   private dino: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  private gameSpeed: number;
 
   constructor() {
     super('PlayScene');
+
+    this.gameSpeed = 10;
   }
 
   create(): void {
@@ -29,10 +32,34 @@ export class PlayScene extends Phaser.Scene {
   handleInputs() {
     // 每次按空格，给予小恐龙一个 -1600 的向上的速度
     this.input.keyboard.on('keydown-SPACE', () => {
-      if(!this.dino.body.onFloor()){
-        return
+      if (!this.dino.body.onFloor()) {
+        return;
       }
+
+      // @ts-ignore
+      this.dino.body.height = 92;
+      this.dino.body.offset.y = 0;
+
       this.dino.setVelocity(-1600);
     });
+
+    // 按下 ↓ 键，小恐龙身体变矮
+    this.input.keyboard.on('keydown-DOWN', () => {
+      // @ts-ignore
+      this.dino.body.height = 58;
+      this.dino.body.offset.y = 34; // 下蹲时 height + offset.y == 92 即身体高度
+    });
+
+    // 松开 ↓ 键，小恐龙身体恢复
+    this.input.keyboard.on('keyup-DOWN', () => {
+      // @ts-ignore
+      this.dino.body.height = 92;
+      this.dino.body.offset.y = 0; // 下蹲时 height + offset.y == 92 即身体高度
+    });
+  }
+
+  // 滚动地图
+  update(time: number, delta: number) {
+    this.ground.tilePositionX += this.gameSpeed;
   }
 }
